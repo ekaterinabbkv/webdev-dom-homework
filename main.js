b3d2ec20d469c623c0c23e057111afe389c641d0
ekaterinabbkv <kir.ekaterina0793@mail.ru>
2023-09-09 23:30:17 +1000

    авторизация

--- main.js
+++ main.js
@@ -1,118 +1,86 @@
-import { getComments } from "./api.js";
-import { postComment } from "./api.js";
-import { renderComments } from "./renderComments.js"
+import { getTodos } from './api.js';
+import { now } from './date.js';
+import { renderComments } from './render.js';
 
-let commentsArr = [];
-
-const buttonElement = document.getElementById("add-button");
-const loadingForm = document.getElementById("loadingForm");
 const loading = document.getElementById("loading");
-const form = document.getElementById("add-form");
-const delFormBtn = document.querySelector(".del-form-button");
-const nameInputElement = document.getElementById("name-input");
-const commentInputElement = document.getElementById("comment-input");
+//const loadingForm = document.getElementById("loadingForm");
+//const addFormButtonElement = document.getElementById("add-form-button");
+// const nameInputElement = document.getElementById("add-form-name");
+// const textInputElement = document.getElementById("add-form-text");
+// const form = document.getElementById("add-form");
+// const deleteCommentButton = document.getElementById("delete-comment-button");
+let quote = "";
+let currentDate = new Date();
+
+now(currentDate);
+
+let comments = [
+];
 
 loading.textContent = "Комментарии загружаются...";
-const fetchGet = () => {
-  getComments().then((responseData) => {
+
+ export const fetchGet = () => {
+  getTodos().then((responseData) => {
     const appComments = responseData.comments.map((comment) => {
       return {
         name: comment.author.name,
         date: now(new Date(comment.date)),
         text: comment.text,
         likes: 0,
-        myLike: false,
-      };
-    });
-    commentsArr = appComments;
+        isLiked: false,
+        changeButton: false,
+      }
+    })
     loading.textContent = "";
-    renderComments({ commentsArr });
+    comments = appComments;
+    renderAllComments();
   });
 };
+
 fetchGet();
 
-buttonElement.disabled = true;
-
-document.addEventListener("input", () => {
-  nameInputElement.value != "" && commentInputElement.value != ""
-    ? (buttonElement.disabled = false)
-    : (buttonElement.disabled = true);
-});
-
-document.addEventListener("keyup", (e) => {
-  if (
-    (e.code === "Enter" || e.code === "NumpadEnter") &&
-    !buttonElement.disabled
-  ) {
-    createNewComment();
-    renderComments({ commentsArr });
-  }
-});
-
-delFormBtn.addEventListener("click", () => {
-  commentsArr.pop();
-  renderComments({ commentsArr });
-  if (!commentsArr) delFormBtn.disabled = true;
-});
-
-
-//renderComments({ commentsArr });
-
-const plusZero = (str) => {
-  return str < 10 ? `0${str}` : str;
-};
-
-const now = (currentDate) => {
-  let minute = plusZero(currentDate.getMinutes());
-  let hours = plusZero(currentDate.getHours());
-  let date = plusZero(currentDate.getDate());
-  let month = plusZero(currentDate.getMonth() + 1);
-  return `${date}.${month}.${
-    currentDate.getFullYear() % 100
-  } ${hours}:${minute}`;
-};
-
-const createNewComment = () => {
-  form.classList.add("none");
-  loadingForm.textContent = "Комментарий добавляется...";
-  // подписываемся на успешное завершение запроса с помощью then
-  const post = (text) => {
-    postComment({
-      name: nameInputElement.value,
-      text: commentInputElement.value,
-    })
-      .then(() => {
-        fetchGet();
-      })
-      .then(() => {
-        loadingForm.textContent = "";
-        form.classList.remove("none");
-        commentInputElement.value = "";
-        nameInputElement.value = "";
-      })
-      .catch((error) => {
-        console.warn(error);
-        if (error.message === "Неверный запрос") {
-          alert("Мало символов");
-        }
-        if (error.message === "Ошибка сервера") {
-          alert("Сервер сломался, попробуйте позже");
-          post(text);
-        }
-        if (window.navigator.onLine === false) {
-          alert("Проблемы с интернетом, проверьте подключение");
-        }
-        loadingForm.textContent = "";
-        form.classList.remove("none");
+const clickLike = () => {
+  const likeButtons = document.querySelectorAll(".like-button");
+  for (const likeButton of likeButtons) {
+    likeButton.addEventListener('click', () => {
+      likeButton.classList.add('-loading-like');
+      function delay(interval = 300) {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve();
+          }, interval);
+        });
+      };
+      delay(2000).then(() => {
+        const newLike = comments[likeButton.dataset.index];
+        newLike.likes = newLike.isLiked
+          ? newLike.likes - 1
+          : newLike.likes + 1;
+        newLike.isLiked = !newLike.isLiked;
+        // newLike.isLikeLoading = false;
+        renderAllComments();
       });
-  };
-  post();
-  renderComments({ commentsArr });
+    })
+  }
 };
 
-buttonElement.addEventListener("click", () => {
-  createNewComment();
-  renderComments({ commentsArr });
-});
+const replyToComment = () => {
+  const commentBodys = document.querySelectorAll(".comment-body");
+  for (const commentBody of commentBodys) {
+    commentBody.addEventListener('click', () => {
+      const oldComment = commentBody.dataset.text;
+      const oldName = commentBody.dataset.name;
+      const quote = `${oldComment}\n${oldName}: `;
+      textInputElement.value += `"${quote}"\n`;
 
-console.log("It works!");
+    })
+  }
+};
+
+ export const renderAllComments = () => {
+  renderComments({ comments });
+  clickLike();
+  replyToComment();
+}
+
+renderAllComments();
